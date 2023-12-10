4cd02445ace6d3add84f97d2c1d1b01125d64e97
Adnane-elKanbouji <adnane1234598@gmail.com>
2023-12-10 13:21:31 +0100

    message

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename='/MiniProjet_react'>
   <Provider store={Store}>
     <App />
     </Provider>
